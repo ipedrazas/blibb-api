b197aea493b25255aec134ecd330c5b440941ac1
Ivan Pedrazas <ipedrazas@gmail.com>
2012-10-06 00:11:13 +0100

    audit push bug

--- API/oiapp/weboi.py
+++ API/oiapp/weboi.py
@@ -94,7 +94,7 @@ def push_oi(oiid=None):
         oi = Oi.get({'_id': ObjectId(oiid)})
         if Oi.in_senders(oi, user):
             Audit.push(user, oi['_id'], '', oi['subscribers'])
-            return jsonify({'push': Oi.push(oiid)})
+            return jsonify({'push': Oi.push(oi)})
         else:
             abort(401)
     abort(400)
