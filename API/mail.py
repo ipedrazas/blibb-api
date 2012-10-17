15992e5876e57c1b493c0370cf3a353851f9f2ea
Ivan Pedrazas <ipedrazas@gmail.com>
2012-10-17 13:23:22 +0100

    mail

--- API/mail.py
+++ API/mail.py
@@ -13,7 +13,7 @@ def send_invites(owner, contacts):
     txt_mail = 'Invitation to Join Oi!'
     html_mail = 'Invitation to Join <b>Oi!</b>'
     subject = owner + " wants to invite you"
-    mail = {'from': "info@oioi.me", 'subject': subject, 'txt_body': txt_mail, 'html_body': html_mail, 'to_name': ''}
+    mail = {'from': "info@oioi.me", 'from_name': 'Oi!', 'subject': subject, 'txt_body': txt_mail, 'html_body': html_mail, 'to_name': ''}
     for p in contacts:
         if is_valid_email(p):
             mail['to_address'] = p
@@ -31,7 +31,7 @@ def send_invitation_mail(mail):
     sendgrid_user = current_app.config.get('SENDGRID_USER')
     sendgrid_password = current_app.config.get('SENDGRID_PASSWORD')
     s = sendgrid.Sendgrid(sendgrid_user, sendgrid_password, secure=True)
-    message = sendgrid.Message(mail['from'], mail['subject'], mail['txt_body'], mail['html_body'])
+    message = sendgrid.Message((mail['from'], mail['from_name']), mail['subject'], mail['txt_body'], mail['html_body'])
     message.add_to(mail['to_address'], mail['to_name'])
 
     s.web.send(message)
