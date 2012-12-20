9c878c5bd52ded0154355f9ee707108d04d8d414
Ivan Pedrazas <ipedrazas@gmail.com>
2012-12-20 23:17:42 +0000

    parse custom action

--- API/oiapp/parse.py
+++ API/oiapp/parse.py
@@ -17,7 +17,7 @@ def do_push(name=None, channel=None, user=None):
 
     data = dict()
 
-    data['data'] = {'alert': user + ': ' + name, 'badge': 'Increment'}
+    data['data'] = {'alert': user + ': ' + name, 'badge': 'Increment', 'action': 'me.oioi.simple.OPENAPP'}
     data["channels"] = [channel]
 
     head = dict()
