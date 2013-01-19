7db826366257c4fb164dac31a18dfff2ec66cb03
Ivan Pedrazas <ipedrazas@gmail.com>
2013-01-19 01:30:59 +0000

    multipart mail worker

--- API/oiapp/models.py
+++ API/oiapp/models.py
@@ -213,7 +213,7 @@ class Oi(Base):
                 oi['_id'] = new_id
                 full_name = cls.get_full_name(oi['owner'])
                 for email in oi['invited']:
-                    queue_mail(new_id, full_name, name, email, comments)
+                    queue_mail(str(new_id), full_name, name, email, comments)
                 return oi
             else:
                 error = {'error': 'Error creating the Oi'}
