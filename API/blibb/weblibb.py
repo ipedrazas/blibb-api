584af2fc168f3a51854ee3dd55311530ade3353c
Ivan Pedrazas <ipedrazas@gmail.com>
2013-02-06 13:12:14 +0000

    short

--- API/blibb/weblibb.py
+++ API/blibb/weblibb.py
@@ -114,7 +114,7 @@ def getBlibb(blibb_id=None, params=None):
 
 @mod.route('/short/<short_id>', methods=['GET'])
 @crossdomain(origin='*')
-def getBlibb(short_id=None, params=None):
+def getBlibbShort(short_id=None, params=None):
     if short_id is None:
         abort(404)
 
