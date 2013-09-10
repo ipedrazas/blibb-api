2a4238782ff0b7e87383ab3c7d36d9b1a8b878fe
Ivan Pedrazas <ipedrazas@gmail.com>
2013-09-10 18:32:34 +0100

    config setting was misconfigured

--- API/contenttypes/webcontent.py
+++ API/contenttypes/webcontent.py
@@ -12,7 +12,8 @@ from API.contenttypes.bookmark import Bookmark
 from API.contenttypes.picture import Picture
 from API.event.event import Event
 from bson.objectid import ObjectId
-from API.utils import get_user_name, allowed_file, is_valid_id, is_image, is_attachment
+from API.utils import get_user_name, allowed_file,  is_valid_id
+from API.utils import is_image, is_attachment, get_config_value
 import API.loader.excel as loader
 
 from API.decorators import crossdomain
@@ -20,7 +21,7 @@ from API.decorators import support_jsonp
 
 from boto import connect_s3
 from boto.s3.key import Key
-import mimetypes
+
 
 mod = Blueprint('content', __name__, url_prefix='')
 
@@ -33,7 +34,7 @@ mod = Blueprint('content', __name__, url_prefix='')
 @crossdomain(origin='*')
 def upload():
     login_key = request.form.get('login_key', False)
-    app_token = request.form.get('app_token', False)
+    # app_token = request.form.get('app_token', False)
     app_user = request.form.get('u', False)
 
     if login_key:
@@ -57,26 +58,27 @@ def upload():
 
         filename = secure_filename(object_id + '-' + file.filename)
         c = connect_s3()
-        bucket_name = current_app.config.get('BUCKET')
+        bucket_name = get_config_value('BUCKET')
         bucket = c.create_bucket(bucket_name)
         k = Key(bucket)
         k.key = user + '/' + filename
         k.set_metadata('owner', user)
         extension = os.path.splitext(filename)[1]
         k.content_type = file.content_type
-        current_app.logger.info('########## Extension: ' + str(extension))
-        current_app.logger.info('########## file.content_type: ' + str(file.content_type))
+        current_app.logger.info('Extension: ' + str(extension))
+        current_app.logger.info('file.content_type: ' + str(file.content_type))
         if extension.lower() == '.jpg':
             k.content_type = 'image/jpeg'
         if extension.lower() == '.png':
             k.content_type = 'image/png'
         if extension.lower() == '.gif':
             k.content_type = 'image/gif'
-        current_app.logger.info('########## Extension: ' + str(extension))
-        current_app.logger.info('########## file.content_type: ' + str(k.content_type))
+        current_app.logger.info('Extension: ' + str(extension))
+        current_app.logger.info('file.content_type: ' + str(k.content_type))
         k.set_contents_from_string(file.read())
         k.make_public()
         url = 'http://%s/%s' % (bucket_name, k.key)
+        current_app.logger.info('########## url: ' + str(url) + ' ' + str(bucket))
         if is_image(file.filename):
             Picture.add_url(object_id, url)
         elif is_attachment(file.filename):
@@ -183,7 +185,7 @@ def newBookmark(song_id=None):
         tags = request.form['tags']
     bk_id = bk.insert(b, user, url, t, bn, tags)
     e.save()
-    return  jsonify({'result': str(bk_id)})
+    return jsonify({'result': str(bk_id)})
 
 ####
 ####
@@ -204,7 +206,7 @@ def loader_excel():
     if file and allowed_file(file.filename):
         try:
             filename = secure_filename(file.filename)
-            excel_file = os.path.join(current_app.config.get('UPLOAD_FOLDER'), filename)
+            excel_file = os.path.join(get_config_value('UPLOAD_FOLDER'), filename)
             file.save(excel_file)
             if is_valid_id(bid):
                 fields = Blibb.get_fields(bid)
